b488a64b014b441f2f4ae821d08111ba8d2b229e
harithakoyyana <95094316+harithakoyyana@users.noreply.github.com>
2022-04-09 18:54:46 -0400

    Add files via upload

---
+++ String1.java
@@ -0,0 +1,7 @@
+public class String1{
+    public static void main(String[] args) {
+        String name = "Envision";
+        System.out.println(name);
+        System.out.println("Length of characters:" + name.length());
+    }
+}
